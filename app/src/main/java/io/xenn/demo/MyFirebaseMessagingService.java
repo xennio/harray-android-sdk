7ba1ccd2cbb7301c852736e8d4be2f1613f66485
Mustafa Simav <mustafa@xenn.io>
2020-03-25 13:50:13 +0300

    Send push feedback events with utm data

--- app/src/main/java/io/xenn/demo/MyFirebaseMessagingService.java
+++ app/src/main/java/io/xenn/demo/MyFirebaseMessagingService.java
@@ -2,7 +2,7 @@ package io.xenn.demo;
 
 
 import com.google.firebase.messaging.FirebaseMessagingService;
-
+import com.google.firebase.messaging.RemoteMessage;
 import io.xenn.android.XennioAPI;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
@@ -11,5 +11,12 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onNewToken(String token) {
         super.onNewToken(token);
         XennioAPI.savePushToken("", token);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        XennioAPI.putPushDeeplink(remoteMessage.getData());
+        XennioAPI.pushReceived();
+        super.onMessageReceived(remoteMessage);
     }
 }
