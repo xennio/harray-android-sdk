633f2c57d0aeb99213e4bb2da2c770604c8a3952
Bay Batu <batuhanbayrakci@gmail.com>
2020-04-24 16:28:00 +0300

    use androidX related test packages with bitmap testing using robolectric

--- android-sdk/src/main/java/io/xenn/android/http/BitmapDownloadTask.java
+++ android-sdk/src/main/java/io/xenn/android/http/BitmapDownloadTask.java
@@ -3,6 +3,10 @@ package io.xenn.android.http;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import com.google.android.gms.common.util.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 import io.xenn.android.utils.XennioLogger;
@@ -15,15 +19,29 @@ public class BitmapDownloadTask {
     }
 
     public Bitmap getBitmap() {
+        InputStream inputStream = null;
         try {
-            return BitmapFactory.decodeStream(new URL(urlString).openConnection().getInputStream());
+            inputStream = new URL(urlString).openConnection().getInputStream();
+            return BitmapFactory.decodeStream(inputStream);
         } catch (Exception e) {
             XennioLogger.log("Bitmap download failed " + e.getMessage());
             return null;
+        } finally {
+            closeStream(inputStream);
         }
     }
 
     protected String getUrlString() {
         return urlString;
+    }
+
+    private void closeStream(InputStream inputStream) {
+        if (inputStream != null) {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                XennioLogger.log("Bitmap input stream closing error " + e.getMessage());
+            }
+        }
     }
 }
--- android-sdk/src/test/java/io/xenn/android/http/BitmapDownloadTaskTest.java
+++ android-sdk/src/test/java/io/xenn/android/http/BitmapDownloadTaskTest.java
@@ -2,19 +2,26 @@ package io.xenn.android.http;
 
 import android.graphics.Bitmap;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockserver.integration.ClientAndServer;
 import org.mockserver.model.MediaType;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Random;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockserver.integration.ClientAndServer.startClientAndServer;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
+@RunWith(AndroidJUnit4.class)
 public class BitmapDownloadTaskTest {
 
     private ClientAndServer mockServer;
@@ -33,18 +40,32 @@ public class BitmapDownloadTaskTest {
     }
 
     @Test
-    public void it_should_return_null_when_url_does_not_contain_bitmap() throws Exception {
+    public void it_should_return_downloaded_bitmap() throws Exception {
+        String imagePath = this.getClass().getClassLoader().getResource("bird.jpg").getFile();
 
         mockServer.when(request()
                 .withMethod("GET")
-                .withPath("/nonimage"))
+                .withPath("/bird.jpg"))
                 .respond(response()
                         .withStatusCode(200)
-                        .withContentType(MediaType.APPLICATION_JSON)
-                        .withBody("aaa".getBytes())
+                        .withContentType(MediaType.JPEG)
+                        .withBody(Files.readAllBytes(Paths.get(imagePath)))
                 );
 
-        BitmapDownloadTask bitmapDownloadTask = new BitmapDownloadTask("http://127.0.0.1:" + serverPort + "/nonimage");
+        BitmapDownloadTask bitmapDownloadTask = new BitmapDownloadTask("http://127.0.0.1:" + serverPort + "/bird.jpg");
+        Bitmap bitmap = bitmapDownloadTask.getBitmap();
+        assertEquals(256, bitmap.getWidth());
+        assertEquals(256, bitmap.getHeight());
+    }
+
+    @Test
+    public void it_should_return_null_when_url_returns_404() {
+        mockServer.when(request()
+                .withMethod("GET")
+                .withPath("/imagenotfound"))
+                .respond(response().withStatusCode(404));
+
+        BitmapDownloadTask bitmapDownloadTask = new BitmapDownloadTask("http://127.0.0.1:" + serverPort + "/imagenotfound");
         Bitmap bitmap = bitmapDownloadTask.getBitmap();
         assertNull(bitmap);
     }
