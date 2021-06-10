9c88af8574bef048b79201453c349a03e8a8172d
Yildirim Adiguzel <yadiguzel@gmail.com>
2021-06-10 23:51:41 +0300

    heart beat removed

--- android-sdk/src/main/java/io/xenn/android/event/SDKEventProcessorHandler.java
+++ android-sdk/src/main/java/io/xenn/android/event/SDKEventProcessorHandler.java
@@ -1,13 +1,7 @@
 package io.xenn.android.event;
 
-import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.LifecycleObserver;
-import androidx.lifecycle.OnLifecycleEvent;
-import androidx.lifecycle.ProcessLifecycleOwner;
 
 import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import io.xenn.android.common.Constants;
 import io.xenn.android.context.ApplicationContextHolder;
@@ -16,7 +10,6 @@ import io.xenn.android.model.XennEvent;
 import io.xenn.android.service.DeviceService;
 import io.xenn.android.service.EntitySerializerService;
 import io.xenn.android.service.HttpService;
-import io.xenn.android.utils.ClockUtils;
 import io.xenn.android.utils.XennioLogger;
 
 public class SDKEventProcessorHandler {
@@ -25,10 +18,7 @@ public class SDKEventProcessorHandler {
     private final HttpService httpService;
     private final EntitySerializerService entitySerializerService;
     private final DeviceService deviceService;
-    private static final Long HEART_BEAT_INTERVAL = 55 * 1000L;
 
-    private Timer timer = new Timer();
-    private TimerTask heartBeatTask;
 
 
     public SDKEventProcessorHandler(ApplicationContextHolder applicationContextHolder, SessionContextHolder sessionContextHolder, HttpService httpService, EntitySerializerService entitySerializerService, DeviceService deviceService) {
@@ -37,7 +27,6 @@ public class SDKEventProcessorHandler {
         this.httpService = httpService;
         this.entitySerializerService = entitySerializerService;
         this.deviceService = deviceService;
-        this.registerProcessLifecycle();
     }
 
     public void sessionStart() {
@@ -77,56 +66,5 @@ public class SDKEventProcessorHandler {
         } catch (Exception e) {
             XennioLogger.log("New Installation error: " + e.getMessage());
         }
-    }
-
-    public void heartBeat() {
-        if (sessionContextHolder.getLastActivityTime() < ClockUtils.getTime() - HEART_BEAT_INTERVAL) {
-            try {
-                Map<String, Object> event = XennEvent.create("HB", applicationContextHolder.getPersistentId(), sessionContextHolder.getSessionIdAndExtendSession())
-                        .memberId(sessionContextHolder.getMemberId())
-                        .toMap();
-                String serializedEntity = entitySerializerService.serializeToBase64(event);
-                httpService.postFormUrlEncoded(serializedEntity);
-
-            } catch (Exception e) {
-                XennioLogger.log("Heart beat error: " + e.getMessage());
-            }
-        }
-    }
-
-    private void registerProcessLifecycle() {
-        ProcessLifecycleOwner.get().getLifecycle().addObserver(
-                new LifecycleObserver() {
-                    @OnLifecycleEvent(Lifecycle.Event.ON_START)
-                    void onMoveToForeground() {
-                        scheduleTimer();
-                    }
-
-                    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
-                    void onMoveToBackground() {
-                        cancelTimer();
-                    }
-                });
-    }
-
-    private void cancelTimer() {
-        timer.purge();
-        timer.cancel();
-        XennioLogger.log("Heart beat task cancelled");
-    }
-
-    private void scheduleTimer() {
-        final SDKEventProcessorHandler processorHandler = this;
-        timer.purge();
-        timer.cancel();
-        timer = new Timer();
-        heartBeatTask = new TimerTask() {
-            @Override
-            public void run() {
-                processorHandler.heartBeat();
-            }
-        };
-        timer.schedule(heartBeatTask, HEART_BEAT_INTERVAL);
-        XennioLogger.log("Heart beat task initialized");
     }
 }
--- android-sdk/src/test/java/io/xenn/android/event/SDKEventProcessorHandlerTest.java
+++ android-sdk/src/test/java/io/xenn/android/event/SDKEventProcessorHandlerTest.java
@@ -16,12 +16,10 @@ import io.xenn.android.context.SessionContextHolder;
 import io.xenn.android.service.DeviceService;
 import io.xenn.android.service.EntitySerializerService;
 import io.xenn.android.service.HttpService;
-import io.xenn.android.utils.ClockUtils;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -92,32 +90,6 @@ public class SDKEventProcessorHandlerTest {
     }
 
     @Test
-    public void it_should_construct_heart_beat_event_and_make_api_call() throws UnsupportedEncodingException {
-
-        ClockUtils.freeze(1587237170000L);
-        ArgumentCaptor<Map<String, Object>> xennEventArgumentCaptor = ArgumentCaptor.forClass(Map.class);
-        when(applicationContextHolder.getPersistentId()).thenReturn("persistentId");
-        when(sessionContextHolder.getSessionIdAndExtendSession()).thenReturn("sessionId");
-        when(sessionContextHolder.getMemberId()).thenReturn("memberId");
-        when(sessionContextHolder.getLastActivityTime()).thenReturn(1584558770000L);
-        when(entitySerializerService.serializeToBase64(xennEventArgumentCaptor.capture())).thenReturn("serializedEntity");
-
-        sdkEventProcessorHandler.heartBeat();
-
-        Map<String, Object> xennEventMap = xennEventArgumentCaptor.getValue();
-        Map<String, Object> header = (Map<String, Object>) xennEventMap.get("h");
-        Map<String, Object> body = (Map<String, Object>) xennEventMap.get("b");
-
-        assertEquals("HB", header.get("n"));
-        assertEquals("sessionId", header.get("s"));
-        assertEquals("persistentId", header.get("p"));
-        assertEquals("memberId", body.get("memberId"));
-
-        verify(httpService).postFormUrlEncoded("serializedEntity");
-        ClockUtils.unFreeze();
-    }
-
-    @Test
     public void it_should_construct_installation_event_and_make_api_call() throws UnsupportedEncodingException {
 
         ArgumentCaptor<Map<String, Object>> xennEventArgumentCaptor = ArgumentCaptor.forClass(Map.class);
@@ -140,14 +112,4 @@ public class SDKEventProcessorHandlerTest {
         verify(httpService).postFormUrlEncoded("serializedEntity");
     }
 
-    @Test
-    public void it_should_not_send_heart_beat_event_when_last_event_time_is_not_before_current_time_minus_interval() throws UnsupportedEncodingException {
-        ClockUtils.freeze(1587237294000L);
-        when(sessionContextHolder.getLastActivityTime()).thenReturn(1587237260000L);
-
-        sdkEventProcessorHandler.heartBeat();
-
-        verifyNoInteractions(httpService);
-        ClockUtils.unFreeze();
-    }
 }
