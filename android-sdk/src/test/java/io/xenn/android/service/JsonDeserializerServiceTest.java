e647303a073b2cc3c7053e800d1778af0c944b99
Batuhan Bayrakci <batuhanbayrakci@gmail.com>
2021-06-30 10:45:39 +0300

    add test for json deserializer

--- android-sdk/src/test/java/io/xenn/android/service/JsonDeserializerServiceTest.java
+++ android-sdk/src/test/java/io/xenn/android/service/JsonDeserializerServiceTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class JsonDeserializerServiceTest {
 
@@ -23,5 +24,16 @@ public class JsonDeserializerServiceTest {
         assertEquals(result.get(0).get("name"), "name1");
         assertEquals(result.get(1).get("id"), "id2");
         assertEquals(result.get(1).get("name"), "name2");
+    }
+
+    @Test
+    public void it_should_deserialize_json_to_map() {
+        Map<String, String> result = service.deserializeToMap(
+                "{ \"id\": \"id1\",  \"name\": \"name1\", \"nullField\": \"null\"  }"
+        );
+
+        assertEquals(result.get("id"), "id1");
+        assertEquals(result.get("name"), "name1");
+        assertNull(result.get("nullField"));
     }
 }
